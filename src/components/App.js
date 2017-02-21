59ddc6d75f2a3398c66727b7f15de7c7922a06dc
pescel <28l.pesce@gmail.com>
2017-02-21 16:49:40 -0700

    boilerplate setup and add app component to make sure it's working

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default class App extends Component{
+  render() {
+  return (
+    <div className='header-container'>
+      <h1 className='header'>ColorPal</h1>
+    </div>
+    )
+  }
+}
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = () => {
+  return (
+    <button className='favorites-btn'>Favorites</button>
+  )
+}
+
+export default Button;
