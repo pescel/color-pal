9e12eade3f85841a8eb53ec6c120bbee92fb6c9b
pescel <28l.pesce@gmail.com>
2017-03-06 19:29:46 -0700

    Updates

--- Tests/Reducers/photo.spec.js
+++
@@ -1,8 +0,0 @@
-import { expect, assert } from 'chai';
-import photo from '../../src/reducers/photo';
-
-describe('photo', () => {
-  it('should return the initial state', () => {
-    expect(photo(undefined, '')).to.deep.equal('');
-  })
-})
